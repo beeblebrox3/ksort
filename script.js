b40e8fa5d34fbf12093a3facd4e0f5d439f69768
Luís Henrique <luish.faria@gmail.com>
2019-01-02 19:45:26 -0200

    clenup

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 (function () {
-	const parent = document.querySelector('#ctn');
-	const getNodes = () => parent.querySelectorAll('.produto-ctn');
-	const getValueFromNode = node => parseInt(node.querySelector('.produto-desconto b').innerText.replace(/[^0-9]+/g, ''), 10);
+    const parent = document.querySelector('#ctn');
 
-	const nodes = getNodes();
-	let temp = [];
+    const getNodes = parent => parent.querySelectorAll('.produto-ctn');
+    const getValueFromNode = node => +node.querySelector('.produto-desconto b').innerText.replace(/[^0-9]+/g, '');
 
-	nodes.forEach(node => {
-		temp.push({
-			desconto: getValueFromNode(node),
-			node,
-		});
-	});
+    const temp = [];
+    getNodes(parent).forEach(node => temp.push({
+        node,
+        discount: getValueFromNode(node),
+    }));
 
-	let before = null;
-	let c = 0;
+    const newParent = parent.cloneNode();
+    getNodes(newParent).forEach(node => node.remove());
 
-	const newParent = parent.cloneNode();
-	newParent.querySelectorAll('.produto-ctn').forEach(node => node.remove());
+    temp.sort((a, b) => b.discount - a.discount)
+        .forEach(node => newParent.appendChild(node.node));
 
-	const sorted = temp.sort((a, b) => b.desconto - a.desconto);
-	console.log(sorted);
-	sorted.forEach(node => {
-		// before && newParent.insertBefore(node.node, before);
-		// before = node.node;
-		newParent.appendChild(node.node);
-	});
-
-	document.querySelector('article').appendChild(newParent);
-	parent.remove();
+    document.querySelector('article').appendChild(newParent);
+    parent.remove();
 }());
